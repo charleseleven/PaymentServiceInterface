d2b6231d77c13640e99ccadb55a9970eed584612
Charles Pires Rodrigues <cpiresrodrigues81@gmail.com>
2021-12-05 09:44:55 -0300

    Adicionar arquivos de projeto.

---
+++ PaymentServiceInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaymentServiceInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
